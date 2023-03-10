cad92befc93ab03e2dffe79582fea823c929001a
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-10 20:46:13 +0100

    Advanced Java

---
+++ src/Generics/classes/GenericClass.java
@@ -0,0 +1,15 @@
+package Generics.classes;
+
+//public class class_name<T>
+public class GenericClass<T> {
+
+    private  T type;
+
+    public T getType() {
+        return type;
+    }
+
+    public void setType(T type) {
+        this.type = type;
+    }
+}
