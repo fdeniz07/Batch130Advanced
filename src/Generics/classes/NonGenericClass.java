a57f5270a3198a64336b58e7b711fb38de8fbb80
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-10 20:46:27 +0100

    Advanced Java

---
+++ src/Generics/classes/NonGenericClass.java
@@ -0,0 +1,16 @@
+package Generics.classes;
+
+public class NonGenericClass {
+
+    //field(özellik):farklı data tiplerini referans alsın
+
+    private Object object;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
