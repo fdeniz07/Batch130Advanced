b5e54b0831f9b9d1fc5db4200ebb7051145cc771
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-11 22:07:42 +0100

    Advanced Java

---
+++ src/enums/TransactionTypeConstant.java
@@ -0,0 +1,12 @@
+package enums;
+
+public class TransactionTypeConstant {
+
+    public static final int DEPOSIT=1;
+    public static final int WITHDRAW=2;
+    public static final int TRANSFER=3;
+    public static final int PAYMENT=4;
+    public static final int OTHER=5;
+
+
+}
