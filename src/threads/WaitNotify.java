692f339d3f56159c890fef4e342e2583ad5fd3e8
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-16 01:52:12 +0100

    Advanced Java

--- src/threads/WaitNotify.java
+++ src/threads/WaitNotify.java
@@ -1,20 +1,20 @@
-package threads;
-
+package Threads;
 /*
-    Bir öğrencinin banka hesabı için para yatırma(deposit) ve çekme işlemleri(withdraw) için uygulama
-    Hesapta para yoksa para yatırılması(bakiyenin artması) beklensin.
-    Bakiye artınca(yeterli olunca) para çekme gerçekleşsin.
+Bir öğrencinin banka hesabı için para yatırma(deposit) ve çekme işlemleri(withdraw) için uygulama
+Hesapta para yoksa para yatırılması(bakiyenin artması) beklensin.
+Bakiye artınca(yeterli olunca) para çekme gerçekleşsin.
+
+wait ve notify monitor edilen(kilitlenen) obje için çağrılır.
 */
-
-
 public class WaitNotify {
-    public static int balance = 0;
+
+    public static int balance=0;// her iki threadde bakiyeyi değiştirmeye çalışıyor.
 
     public static void main(String[] args) {
 
-        WaitNotify obj = new WaitNotify();
+        WaitNotify obj=new WaitNotify();
 
-        Thread thread1 = new Thread(new Runnable() {
+        Thread thread1=new Thread(new Runnable() {
             @Override
             public void run() {
                 obj.withdraw(1000);
@@ -23,48 +23,57 @@ public class WaitNotify {
         thread1.setName("ogrenci");
         thread1.start();
 
-        Thread thread2 = new Thread(new Runnable() {
+        Thread thread2=new Thread(new Runnable() {
             @Override
             public void run() {
-                obj.deposit(2000);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }//sonucu görebilmek için
+                obj.deposit(500);
             }
         });
         thread2.setName("veli");
         thread2.start();
 
+
     }
 
+
     //para çekme işlemi için metod oluşturalım
-    public synchronized void withdraw(int amount) {
-        System.out.println(Thread.currentThread().getName() + " para çekmek istiyor.");
-        if (balance <= 0 || balance < amount) {
-            System.out.println("Bakiye yetersiz...Mevcut bakiye :" + balance);
+    public synchronized void withdraw(int amount){
+        System.out.println(Thread.currentThread().getName()+" para çekmek istiyor.");
+        if(balance<=0 || balance<amount){
+            System.out.println("Bakiye yetersiz...Mevcut bakiye :"+balance);
             System.out.println("Bakiyenin güncellenmesini bekliyor.");
             try {
-                wait();
+                wait();//Object classının static metodu wait metodu hangi thread üzerinde ise thread geçici olarak beklemeye alınır.
+                       //obj nesnesi geçici serbest bırakılıyor.
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
 //bakiye yeterli ise
-        if (balance >= amount) {
-            balance = balance - amount;
-            System.out.println("Para çekme işlemi gerçekleşti... Mevcut bakiye : " + balance);
-        } else {
-            System.out.println("Bakiye yetersiz...Mevcut bakiye :" + balance);
+        if(balance>=amount){
+            balance=balance-amount;
+            System.out.println("Para çekme işlemi gerçekleşti... Mevcut bakiye : "+balance);
+        }else{
+            System.out.println("Bakiye yetersiz...Mevcut bakiye :"+balance);
             System.out.println("Umudunu kaybetme, yarın yine gel:)");
         }
     }
-
     //para yatırma metodu
-    public synchronized void deposit(int amount) {
-        System.out.println(Thread.currentThread().getName() + " para yatırmak istiyor...");
-        balance = balance + amount;
-        System.out.println("Para yatırma işlemi gerçekleşti...Mevcut bakiye:" + balance);
-        notify();
+    public synchronized void deposit(int amount){
+        System.out.println(Thread.currentThread().getName()+" para yatırmak istiyor...");
+        balance=balance+amount;
+        System.out.println("Para yatırma işlemi gerçekleşti...Mevcut bakiye:"+balance);
+        //bakiye güncelleme yapıldığı için diğer threade haber gönderiyor.
+        notify();//obj serbest bırakmaz.
         System.out.println();
         System.out.println("notify metodu çağırılınca hemen kilidi serbest bırakmıyor metodun tamamlanmasını bekler. ");
     }
+
 
 
 }
